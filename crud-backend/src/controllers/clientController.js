76b164f78bb12d0faec39b3ada9adf3102877c10
McCarthyCharles233 <charlesmccarthy233@gmail.com>
2024-11-25 01:36:22 +0000

    added the create functionality

--- crud-backend/src/controllers/clientController.js
+++ crud-backend/src/controllers/clientController.js
@@ -4,8 +4,20 @@ export const getClients = async (req, res) => {
     try {
         const clients = await clientService.getClients();
         res.status(200).json(clients);
-    } catch (err) {
+    } catch (error) {
         console.error("Error fetching clients:", error);
-        res.status(200).json({message: 'Internal Server'});
+        res.status(400).json({message: 'Internal Server'});
     }
 };
+
+export const createClient = async (req, res) => {
+    try {
+        const clientData = req.body;
+        const newClient = await clientService.createClient(clientData);
+        res.status(200).json(newClient);
+    } catch (error) {
+        console.error("Error fetching clients:", error);
+        res.status(500).json({message: 'Internal Server'});
+    }
+};
+
--- crud-backend/src/services/clientService.js
+++ crud-backend/src/services/clientService.js
@@ -4,3 +4,15 @@ export const getClients = async() => {
     const {rows} = await query("SELECT * FROM clients_tb");
     return rows;
 }
+
+export const createClient = async(clientData) => {
+    const {name, email, job, isactive} = clientData;
+
+    const {rows} = await query(
+        "INSERT INTO clients_tb(name, email, job, isactive) VALUES ($1, $2, $3, $4) RETURNING *",
+        [name, email, job, isactive]
+    );
+
+    return rows[0];
+}
+
