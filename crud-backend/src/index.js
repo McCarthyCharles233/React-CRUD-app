1f8b45976905b11e49fc4db9dda1dfdd240baaac
McCarthyCharles233 <charlesmccarthy233@gmail.com>
2024-11-30 02:44:40 +0000

    added a docker-compose file and fixed issues

--- crud-backend/src/index.js
+++ crud-backend/src/index.js
@@ -6,10 +6,15 @@ import clientRoutes from './routes/clientRoute.js'
 const app = express();
 const port = 3000;
 
+app.use(cors({ origin: '*' }));
+
 app.use(cors());
 app.use(express.json());
 
 app.use('/api', clientRoutes)
+app.use('/', (req, res) => {
+    res.send("hello world");
+});
 
 app.listen(port, () => {
     console.log('listening on port 3000')
--- crud-frontend/vite.config.js
+++ crud-frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  define: {
+    'import.meta.env.VITE_BACKEND_URL': JSON.stringify(
+      process.env.VITE_BACKEND_URL || 'http://localhost:3000'
+    )
+  },
+
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
 })
